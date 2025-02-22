34922b1a493c67c13a734cf54f69c72416b8b2f0
Mohd Parvez Ansari <mdparvezansari91@gmail.com>
2025-02-22 09:00:07 +0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,12 +25,12 @@ export const metadata: Metadata = {
     siteName: 'Next.js',
     images: [
       {
-        url: 'https://nextjs.org/og.png', // Must be an absolute URL
+        url: 'https://images.unsplash.com/photo-1730461749413-60eb706bd897?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', // Must be an absolute URL
         width: 800,
         height: 600,
       },
       {
-        url: 'https://nextjs.org/og-alt.png', // Must be an absolute URL
+        url: 'https://images.unsplash.com/photo-1730461749413-60eb706bd897?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', // Must be an absolute URL
         width: 1800,
         height: 1600,
         alt: 'My custom alt',
